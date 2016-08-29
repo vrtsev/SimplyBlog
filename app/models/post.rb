9fb2a92602070d5880496b1c0e82e0f765629d6b
vrtsev <v.rtsev@gmail.com>
2016-08-29 21:24:38 +0300

    Fixed validations, page-titles, about page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	belongs_to :user
 
-	validates :title, length: { minimum: 3}, presence: true
+	validates :title, length: { minimum: 3, maximum: 60}, presence: true
 	validates :content, length: { minimum: 3}, presence: true
 
 end
